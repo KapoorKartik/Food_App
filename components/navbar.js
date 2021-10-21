4cde0abe114353aa09d5e2219b6f8962c377e6c8
KapoorKartik <kapoorkartik110@gmail.com>
2021-10-21 08:10:31 +0530

    debounce and nav bar complete

---
+++ components/navbar.js
@@ -0,0 +1,24 @@
+
+function navbar() {
+    return `<div id="logo">
+    <img
+      src="https://i.pinimg.com/originals/01/48/66/0148662247327a7d268ce7d02b8662cf.png"
+      alt=""
+    />
+  </div>
+  <button id="search_btn">
+  <a href="search.html">Search Recipe</a>
+  
+  </button>
+  
+  <button id="day_recipe">
+  <a href="recipe_of_the_day.html">Recipe of the day</a>
+  
+  </button>
+
+  <button id="latest_recipe">
+  <a href="latest_recipe.html">Latest Recipes</a></button>
+  <button class="material-icons-outlined">account_circle</button>`
+  }
+
+  export default navbar
--- scripts/search.js
+++ scripts/search.js
@@ -1,6 +1,29 @@
 async function searchRecipe(url) {
-    let res = await fetch(url)
-    let data = await res.json()
-    return data
+  let res = await fetch(url);
+  let data = await res.json();
+
+return data.meals
 }
-export  {searchRecipe}
+var timerID;
+
+function debounce(func, delay) {
+  if (timerID) {
+    clearTimeout(timerID);
+  }
+  timerID = setTimeout( () => {
+    func();
+  }, delay);
+}
+ async function appendName(data ,cont) {
+  cont.innerHTML = null
+data.forEach(({strMeal}) => {
+  let p = document.createElement('p')
+        p.innerHTML = strMeal
+        cont.append(p)
+})
+
+}
+
+
+
+export { searchRecipe , debounce , appendName };
